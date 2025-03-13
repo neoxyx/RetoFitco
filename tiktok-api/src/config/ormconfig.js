40bfcbfecebac09c82f858d034d2e1143ae8ff32
Jhon <jhon.valdes@softgic.co>
2025-03-13 12:56:14 -0500

    Ajuste orm

--- tiktok-api/src/config/ormconfig.js
+++ tiktok-api/src/config/ormconfig.js
@@ -1,9 +1,10 @@
+require("dotenv").config();
 const { DataSource } = require("typeorm");
 
 const AppDataSource = new DataSource({
   type: "mysql",
   host: process.env.DB_HOST,
-  port: process.env.DB_PORT,
+  port: Number(process.env.DB_PORT),
   username: process.env.DB_USER,
   password: process.env.DB_PASS,
   database: process.env.DB_NAME,
